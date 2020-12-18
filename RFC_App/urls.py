5423f59c9038b672841d5cb68434764bf71ff4dd
James Alts <james.altheide@gmail.com>
2020-12-18 12:51:51 -0800

    1. updated navbar with landing page link 2. integrated login/reg w/ crispy forms - user authentication looks to be working properly, might have to tweak what's user inforation is stored in session as needed

--- RFC_App/urls.py
+++ RFC_App/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='landingPage'),
+    path('account/create/', views.signup, name='signup'),
+    path('account/signin/', views.signin, name='signin'),
+    path('account/signout/', views.signout, name='signout')
 ]
--- RFC_App/views.py
+++ RFC_App/views.py
@@ -1,4 +1,43 @@
 from django.shortcuts import render,redirect
+from .models import *
+from django.contrib import messages
+from django.contrib.auth.models import Group, User
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 def home(request):
     return render(request,"landingPage.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            signup_user = User.objects.get(username=username)
+            #customer_group = Group.objects.get(name='Customer')
+            #customer_group.user_set.add(signup_user)
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST) #yikes, on that data per parameter for authform
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('landingPage')
+            else:
+                return redirect('signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signin.html', {'form': form})
+
+def signout(request):
+    logout(request)
+    return redirect('signin')

287758e1959a9dc82269af9a1e4d1be09ef72553
James Alts <james.altheide@gmail.com>
2020-12-18 21:28:52 -0800

    added MVC forum w/ posts and comments on their own pages

--- RFC_App/urls.py
+++ RFC_App/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('account/signin/', views.signin, name='signin'),
     path('account/signout/', views.signout, name='signout'),
     path('forum/', views.forum, name='forum'),
+    path('add_post', views.add_post, name='add_post'),
+    path('add_comment/<int:id>', views.add_comment),
+    path('goto_post/<int:id>', views.single_post_page),
 ]

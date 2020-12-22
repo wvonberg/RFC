4f7f287d15df4f9c86df71cc8616a2a9bc8b87bb
Nate Birth <nate.birth@gmail.com>
2020-12-21 18:27:24 -0700

    Speak easy logic

--- RFC_App/admin.py
+++ RFC_App/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Post, Support
+from .models import Post, Support, Profile, Comment, Request, Profile
 
 admin.site.register(Post)
+admin.site.register(Comment)
 admin.site.register(Support)
+admin.site.register(Request)
+admin.site.register(Profile)
